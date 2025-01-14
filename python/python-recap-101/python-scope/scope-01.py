22d3b862d22328399012ac267c27df25985f4108
Mahboob-A <iammahboob.a@gmail.com>
2025-01-14 17:40:45 +0530

    scope in python

--- python/python-recap-101/python-scope/scope-01.py
+++ python/python-recap-101/python-scope/scope-01.py
@@ -3,15 +3,16 @@
 
 
 import http, math
+import builtins 
 
-# the globals tells us what is in the global scope of this current file 
+# the globals tells us what is in the global scope of this current file
 def print_global_scope(): 
         print(globals())
         print()        
 
 # print_global_scope()
 
-# if we want to know what a module offers, or the scope of the module, we 
+# if we want to know what a module offers, or the scope of the module, we
 # can use the dir() function or the __dict__ attribute.
 def print_what_a_module_has(): 
         print(dir(http))
@@ -29,9 +30,9 @@ def see_what_a_function_has(name=None):
         name2 = "doe"
         age = 30 
         print("name: ", name + " ", name2, "\nage: ", age)
-        
+
 # see_what_a_function_has(name="john")
-# using __code__.co_ we can inspect the scope, names, args etc of a function 
+# using __code__.co_ we can inspect the scope, names, args etc of a function
 # print(see_what_a_function_has.__code__)
 print(see_what_a_function_has.__code__.co_varnames)
 print(see_what_a_function_has.__code__.co_code)
@@ -42,3 +43,14 @@ print(see_what_a_function_has.__code__.co_names)
 print(see_what_a_function_has.__code__.co_filename)
 
 # print(dir(see_what_a_function_has))
+
+# print("\n\n")
+# print(dir(builtins))
+
+
+# the regular for loops, the loop variable holds the last value processed by the loop after the loop ended. 
+for item in range(1, 6):
+        print(item)
+
+print()
+print(item)        
