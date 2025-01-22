79fd12d4372d41c9938cc257e1bc6d8c89926369
Mahboob-A <iammahboob.a@gmail.com>
2025-01-22 08:19:40 +0530

    duck typnig recap

---
+++ python/python-recap-101/python-type-checking/duck-typing.py
@@ -0,0 +1,18 @@
+# 220125, Wednesday, 08.00 AM 
+# recap python duck typing 
+
+
+
+# dynamically typed. as we do not need to specify the type of the variable just like
+# c or c++ or java. 
+# as c type language needs to specify the type of the variable at the compile time, 
+# but in python the type is dynamically decided at the run time or interpreter time.
+# as we do not need to specify the type of the variable, this is called as dynamically typed language.
+# and as we need to specify the type of  the variable at the compile time for c type language, we call it as statically typed language.
+def square(x):
+    return x * x
+
+
+
+
+
